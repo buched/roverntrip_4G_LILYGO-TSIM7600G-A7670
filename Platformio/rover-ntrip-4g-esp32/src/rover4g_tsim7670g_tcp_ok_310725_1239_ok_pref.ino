02fa34da5e67be5eae6ae48b2e2d9a141d7a5fbc
buched <32975584+buched@users.noreply.github.com>
2025-08-03 18:05:08 +0200

    Update rover4g_tsim7670g_tcp_ok_310725_1239_ok_pref.ino

--- Platformio/rover-ntrip-4g-esp32/src/rover4g_tsim7670g_tcp_ok_310725_1239_ok_pref.ino
+++ Platformio/rover-ntrip-4g-esp32/src/rover4g_tsim7670g_tcp_ok_310725_1239_ok_pref.ino
@@ -1,5 +1,34 @@
 // base du code : Julien ANCELIN. ajout du mode de transmission UDP,BT,BLE et TCP : Buched, interface de configuration web
+/*
+=============================================================================================
+* By: HnV CAD / Herve JOLIVET 
+  Date: June 17th, 2025
+  License: MIT.
 
+* Souce: https://github.com/jancelin/physalia
+* By: INRAE / Julien Ancelin & Romain Tourte, Quentin Lesniack
+* License: GNU Affero General Public License v3.0
+
+* Object: 
+  - Connect to 4g LTE network and get RTCM data from CentipedeRTK caster as a Client with web interface control / compteur 
+  - Transmit Lat long positions to the caster for base selection automatique
+
+* Material:
+  - ESP32 with Pcie + RJ45: Lilygo T-pcie  =-=-= INFO : LILYGO ® Carte Bluetooth Wifi TTGO T-Internet-COM ESP32 pour Module IOT Ethernet T-PCIE avec emplacement pour carte SIM TF connecteur type-c    
+      https://fr.aliexpress.com/item/1005003547423153.html?spm=a2g0o.order_detail.order_detail_item.3.4eeb7d56Nkpwmr&gatewayAdapt=glo2fra
+  - LTE 4G: only SIM T-PCIE A7670E not GPS =-=-=  INFO : LILYGO® TTGO T-PCIE A7670 4G Carte de Développement ESP32-WROVER-B WIFI Bluetooth epiSeries Composable Tech A7670E A7670SA persévérance 101
+      https://www.tinytronics.nl/shop/en/communication-and-signals/wireless/gps/modules/lilygo-ttgo-t-pcie-sim7600g-h-expansion-module
+
+  - F9P: drotek DP0601                    https://store-drotek.com/891-rtk-zed-f9p-gnss.html
+  - relay: 5V 1-channel high-active       https://www.tinytronics.nl/shop/en/switches/relays/5v-relay-1-channel-high-active
+  - battery: Li-Po Battery 3.7V 2000mAh   https://www.tinytronics.nl/shop/en/power/batteries/li-po/li-po-accu-3.7v-2000mah
+  
+* GNSS code:
+  By: SparkFun Electronics / Nathan Seidle & Paul Clark
+  Date: January 13th, 2022
+  License: MIT.
+=============================================================================================
+*/
 // Select your modem:
 #define TINY_GSM_MODEM_SIM7600  // https://lilygo.cc/products/a-t-pcie + https://lilygo.cc/products/a-t-pcie?variant=42335922028725
 //#define TINY_GSM_MODEM_A7670  // https://lilygo.cc/products/t-sim-a7670e?variant=42737494458549
@@ -43,55 +72,8 @@ unsigned long countRtcm = 0;
 unsigned long startData = 0;
 unsigned long totalData = 0;
 
-int GNSS_FREQ = 1;
-
-// DEEP SLEEP CONFIGURATION
-RTC_DATA_ATTR int bootCount = 0;    // Compte le nombre de reboot. 
-int nb_DeepSleep_until_Reboot = 10; // nb de deepsleep avant reboot complet.
-bool DEEP_SLEEP_ACTIVATED = false; //true;     // True = DeepSleep sinon DeepSleep ( off ) captation en continue
-int TIME_TO_SLEEP = 5; //480 // temps de repos en deepsleep.
-int RTK_ACQUISITION_PERIOD = 30; //120; // Temps ( en seconde ) pendant lequel on doit capter de la donnée en RTK ( secondes )
-int RTK_MAX_RESEARCH = 30;//120; // Temps max pendant lequel le dispositif recherche du RTK ( secondes )
-#define uS_TO_S_FACTOR 1000000
-RTC_DATA_ATTR int lastPeriodRecord = 0;
 int ACQUISION_PERIOD_4G = 120; // Temps ( en seconde ) pendant lequel on va chercher le network 4G avant de faire un deepsleep( TIME_TO_SLEEP )
-int ACQUISION_PERIOD_MQTT = 30000; // Temps d'acquisition pendant lequel on va chercher le serveur mqtt
-int ACQUISION_PERIOD_GNSS = 30000; // Temps d'acquisition pendant lequel on va chercher le serveur mqtt
 
-// BAT
-int BAT_PERIOD = 10;    // Interval pour envoi de l'état de la batterie (en seconde )
-
-
-/*
-=============================================================================================
-* By: HnV CAD / Herve JOLIVET 
-  Date: June 17th, 2025
-  License: MIT.
-
-* Souce: https://github.com/jancelin/physalia
-* By: INRAE / Julien Ancelin & Romain Tourte, Quentin Lesniack
-* License: GNU Affero General Public License v3.0
-
-* Object: 
-  - Connect to 4g LTE network and get RTCM data from CentipedeRTK caster as a Client with web interface control / compteur 
-  - Transmit Lat long positions to the caster for base selection automatique
-
-* Material:
-  - ESP32 with Pcie + RJ45: Lilygo T-pcie  =-=-= INFO : LILYGO ® Carte Bluetooth Wifi TTGO T-Internet-COM ESP32 pour Module IOT Ethernet T-PCIE avec emplacement pour carte SIM TF connecteur type-c    
-      https://fr.aliexpress.com/item/1005003547423153.html?spm=a2g0o.order_detail.order_detail_item.3.4eeb7d56Nkpwmr&gatewayAdapt=glo2fra
-  - LTE 4G: only SIM T-PCIE A7670E not GPS =-=-=  INFO : LILYGO® TTGO T-PCIE A7670 4G Carte de Développement ESP32-WROVER-B WIFI Bluetooth epiSeries Composable Tech A7670E A7670SA persévérance 101
-      https://www.tinytronics.nl/shop/en/communication-and-signals/wireless/gps/modules/lilygo-ttgo-t-pcie-sim7600g-h-expansion-module
-
-  - F9P: drotek DP0601                    https://store-drotek.com/891-rtk-zed-f9p-gnss.html
-  - relay: 5V 1-channel high-active       https://www.tinytronics.nl/shop/en/switches/relays/5v-relay-1-channel-high-active
-  - battery: Li-Po Battery 3.7V 2000mAh   https://www.tinytronics.nl/shop/en/power/batteries/li-po/li-po-accu-3.7v-2000mah
-  
-* GNSS code:
-  By: SparkFun Electronics / Nathan Seidle & Paul Clark
-  Date: January 13th, 2022
-  License: MIT.
-=============================================================================================
-*/
 #ifdef TINY_GSM_MODEM_SIM7600
   // Configuration des broches pour modele original SIM7600
   #define UART_BAUD 115200   // for modem only
@@ -109,11 +91,7 @@ int BAT_PERIOD = 10;    // Interval pour envoi de l'état de la batterie (en sec
   #define BOARD_POWERON  12
   #define BAT_ADC_PIN 35
 #endif
-//#define LED_PIN   12
-//#define POWER_PIN 25
-//#define IND_PIN   36
-// BAT
-//#include <esp_adc_cal.h>
+
 float lastBatPercent = 0.0;
 float lastBatVoltage = 0.0;
 int vref = 1100;
@@ -131,9 +109,6 @@ HardwareSerial GNSSSerial(2);      // UART2 - GNSS
 #define GNSSBAUD 460800
 #define GNSS_TX    32 //=> vers RX LG580P
 #define GNSS_RX    33 // => vers TX LG580P
-
-// See all AT commands, if wanted
-//#define DUMP_AT_COMMANDS
 
 // Define the serial console for debug prints, if needed
 #define TINY_GSM_DEBUG Serial
@@ -793,11 +768,14 @@ while (Serial.available()) // Empty the serial buffer
 void loop()
 {
   server.handleClient();
+    #ifdef TINY_GSM_MODEM_SIM7600
+  #elif defined(TINY_GSM_MODEM_A7670)
   static unsigned long lastBatRead = 0;
   if (millis() - lastBatRead > 5000) {
     readBatteryPercent();
     lastBatRead = millis();
   }
+#endif
   static bool ggaOk = false;
 
   // =-=-=-=-= GNSS READ STATE =-=-=-=-=
