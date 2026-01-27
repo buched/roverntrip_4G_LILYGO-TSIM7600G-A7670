0947ac9f8a5bc4febd2acef8cb2198d674695186
buched <32975584+buched@users.noreply.github.com>
2026-01-27 07:23:12 +0100

    Swap GNSS TX and RX pin definitions

--- arduino/rover4g_tsim7670g.ino
+++ arduino/rover4g_tsim7670g.ino
@@ -81,8 +81,8 @@ HardwareSerial SerialAT(1);  // UART1 - Modem
 HardwareSerial GNSSSerial(2);      // UART2 - GNSS
 
 #define GNSSBAUD 460800
-#define GNSS_TX    34 //=> vers RX LG580P
-#define GNSS_RX    32 // => vers TX LG580P
+#define GNSS_TX    32 //=> vers RX LG580P
+#define GNSS_RX    34 // => vers TX LG580P
 
 // Define the serial console for debug prints, if needed
 #define TINY_GSM_DEBUG Serial
